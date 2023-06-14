6a5f4890277af82ce79d77cf2aa84b024cf943c2
does <ldk980130@gmail.com>
2023-06-14 22:59:05 +0900

    chore: 프로젝트 세팅

---
+++ src/main/java/com/practice/concurrencycontrolredis/ConcurrencyControlRedisApplication.java
@@ -0,0 +1,13 @@
+package com.practice.concurrencycontrolredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrencyControlRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurrencyControlRedisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/concurrencycontrolredis/ConcurrencyControlRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.concurrencycontrolredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrencyControlRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
