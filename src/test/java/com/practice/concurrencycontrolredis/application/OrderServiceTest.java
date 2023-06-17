757e1c23a1a8a058f320d43aa7b10fe23301fa0b
does <ldk980130@gmail.com>
2023-06-17 18:07:41 +0900

    test: 동시성 테스트 작성

--- src/test/java/com/practice/concurrencycontrolredis/application/OrderServiceTest.java
+++ src/test/java/com/practice/concurrencycontrolredis/application/OrderServiceTest.java
@@ -12,6 +12,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -74,6 +77,71 @@ class OrderServiceTest {
                 () -> assertThat(friedStock).isEqualTo(45),
                 () -> assertThat(ballStock).isEqualTo(97)
         );
+    }
 
+    @Test
+    void 동시에_60개의_주문을_처리한다() throws InterruptedException {
+        // given
+        int requestCount = 60;
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        CountDownLatch latch = new CountDownLatch(requestCount);
+
+        OrderRequest request1 = new OrderRequest(List.of(
+                new OrderItemRequest(spicyChickenId, 1),
+                new OrderItemRequest(cheeseBallId, 3)
+        ));
+        OrderRequest request2 = new OrderRequest(List.of(
+                new OrderItemRequest(spicyChickenId, 1),
+                new OrderItemRequest(friedChickenId, 1),
+                new OrderItemRequest(cheeseBallId, 1)
+        ));
+        OrderRequest request3 = new OrderRequest(List.of(
+                new OrderItemRequest(spicyChickenId, 1),
+                new OrderItemRequest(cheeseBallId, 4)
+        ));
+
+        // when
+        for (int i = 0; i < 20; i++) {
+            executorService.submit(() -> {
+                try {
+                    orderService.order(request1);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+        for (int i = 0; i < 20; i++) {
+            executorService.submit(() -> {
+                try {
+                    orderService.order(request2);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+        for (int i = 0; i < 20; i++) {
+            executorService.submit(() -> {
+                try {
+                    orderService.order(request3);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        // then
+        Integer spicyStock = itemRepository.findById(spicyChickenId).orElseThrow().getStock();
+        Integer friedStock = itemRepository.findById(friedChickenId).orElseThrow().getStock();
+        Integer ballStock = itemRepository.findById(cheeseBallId).orElseThrow().getStock();
+        int successOrderCount = orderRepository.findAll().size();
+        assertAll(
+                () -> assertThat(spicyStock).isEqualTo(0),
+                () -> assertThat(friedStock).isEqualTo(30),
+                () -> assertThat(ballStock).isEqualTo(0),
+
+                () -> assertThat(successOrderCount).isEqualTo(45)
+        );
     }
 }
